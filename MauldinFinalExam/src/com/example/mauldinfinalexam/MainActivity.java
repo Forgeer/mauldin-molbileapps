a4a9b8eb91d088dcb3d99f27e9977caf8591f670
unknown <JakeMauldin18@jakemauldin18.MaristSchool.Local>
2015-03-02 14:06:06 -0500

    MauldinFinalExam

--- MauldinFinalExam/src/com/example/mauldinfinalexam/MainActivity.java
+++ MauldinFinalExam/src/com/example/mauldinfinalexam/MainActivity.java
@@ -4,14 +4,50 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	
+	private EditText editDollars;
+	private EditText editYen;
+	private EditText editPounds;
+	private Button buttonDollars;
+	private Button buttonYen;
+	private Button buttonPounds;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		editDollars = (EditText)findViewById(editDollars);
+		editYen = (EditText)findViewById(editYen);
+		editPounds = (EditText)findViewById(editPounds);
+		buttonDollars = (Button)findViewById(buttonDollars);
+		buttonYen = (Button)findViewById(buttonYen);
+		buttonPounds = (Button)findViewById(buttonPounds);
+		
 	}
+
+	private Button findViewById(Button buttonDollars2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private EditText findViewById(EditText editPounds2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public void getText(){
+		
+		
+	}
+		
+	
+	
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,6 +55,18 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	private String getDollars(){
+		double dollar = Double.valueOf(editDollars.getText().toString());
+		double yen = dollar * (getText(editYen)/1.62);
+		double pounds = dollar * (getText(editPounds)/0.0103);
+		String dollarString = String.valueOf(yen, pounds);
+		return dollarString;
+		
+		
+	}
+	
+	
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
